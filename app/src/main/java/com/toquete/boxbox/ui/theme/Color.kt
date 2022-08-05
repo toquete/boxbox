bc9ab7612a25d05b46fefe0272a497b6da308213
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-08-05 15:27:17 -0300

    Defining theme colors

--- app/src/main/java/com/toquete/boxbox/ui/theme/Color.kt
+++ app/src/main/java/com/toquete/boxbox/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.toquete.boxbox.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Red200 = Color(0xFFE7968F)
+val Red500 = Color(0xFFCF2D1E)
+val Red700 = Color(0xFFC32216)
+val Black200 = Color(0xFF808080)
+val Black500 = Color(0xFF000000)
--- app/src/main/java/com/toquete/boxbox/ui/theme/Theme.kt
+++ app/src/main/java/com/toquete/boxbox/ui/theme/Theme.kt
@@ -7,24 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Red200,
+    primaryVariant = Red700,
+    secondary = Black500
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Red500,
+    primaryVariant = Red700,
+    secondary = Black200
 )
 
 @Composable
