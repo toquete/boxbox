c8d9696035a1b694da3d1e58fb400c83b3468225
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-08-11 11:40:49 -0300

    Creating Main TopAppBar

--- app/src/main/java/com/toquete/boxbox/MainActivity.kt
+++ app/src/main/java/com/toquete/boxbox/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.toquete.boxbox
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.toquete.boxbox.ui.theme.BoxBoxTheme
+import com.toquete.boxbox.ui.theme.primaryLightDark
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +24,7 @@ class MainActivity : ComponentActivity() {
             BoxBoxTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Main()
                 }
             }
         }
@@ -27,14 +32,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Main() {
+    TopAppBar {
+        Image(
+            painter = painterResource(id = R.drawable.ic_f1),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(MaterialTheme.colors.primaryLightDark)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Main Light", showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun MainLightPreview() {
     BoxBoxTheme {
-        Greeting("Android")
+        Main()
+    }
+}
+
+@Preview(name = "Main Dark", showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun MainDarkPreview() {
+    BoxBoxTheme {
+        Main()
     }
 }
--- app/src/main/java/com/toquete/boxbox/ui/theme/Theme.kt
+++ app/src/main/java/com/toquete/boxbox/ui/theme/Theme.kt
@@ -1,10 +1,12 @@
 package com.toquete.boxbox.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Red200,
@@ -17,6 +19,8 @@ private val LightColorPalette = lightColors(
     primaryVariant = Red700,
     secondary = Black200
 )
+
+val Colors.primaryLightDark: Color get() = if (isLight) onPrimary else primary
 
 @Composable
 fun BoxBoxTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
