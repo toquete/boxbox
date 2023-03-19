a3bc8f4fd0ff075320aa8197bc3c808849170b46
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-03-19 12:02:29 -0300

    Removing unnecessary dispatcher

--- app/src/main/java/com/toquete/boxbox/data/repository/DriverStandingsRepositoryImpl.kt
+++ app/src/main/java/com/toquete/boxbox/data/repository/DriverStandingsRepositoryImpl.kt
@@ -3,19 +3,13 @@ package com.toquete.boxbox.data.repository
 import com.toquete.boxbox.data.source.DriverStandingsDataSource
 import com.toquete.boxbox.domain.model.DriverStanding
 import com.toquete.boxbox.domain.repository.DriverStandingsRepository
-import com.toquete.boxbox.infrastructure.di.IoDispatcher
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class DriverStandingsRepositoryImpl @Inject constructor(
-    private val dataSource: DriverStandingsDataSource,
-    @IoDispatcher private val dispatcher: CoroutineDispatcher
+    private val dataSource: DriverStandingsDataSource
 ) : DriverStandingsRepository {
 
     override suspend fun getDriverStandings(): List<DriverStanding> {
-        return withContext(dispatcher) {
-            dataSource.getDriverStandings()
-        }
+        return dataSource.getDriverStandings()
     }
 }
