0630f838a0bb0dced0c1b31220d40cc2131a556f
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-04-01 16:47:14 -0300

    Removing plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,7 @@
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("boxbox.android.application")
     id("boxbox.android.application.compose")
     id("boxbox.android.hilt")
-    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
