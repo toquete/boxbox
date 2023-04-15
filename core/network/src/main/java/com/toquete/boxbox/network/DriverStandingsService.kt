fb019ba29f53dfba51d61093ab5ee8ab86858869
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-04-15 19:49:53 -0300

    Creating constructor standings network call

--- core/network/src/main/java/com/toquete/boxbox/network/DriverStandingsService.kt
+++ core/network/src/main/java/com/toquete/boxbox/network/DriverStandingsService.kt
@@ -1,5 +1,6 @@
 package com.toquete.boxbox.network
 
+import com.toquete.boxbox.network.model.ConstructorStandingsWrapper
 import com.toquete.boxbox.network.model.DriverStandingsWrapper
 import retrofit2.http.GET
 
@@ -7,4 +8,7 @@ interface DriverStandingsService {
 
     @GET("current/driverStandings.json")
     suspend fun getDriverStandings(): DriverStandingsWrapper
+
+    @GET("current/constructorStandings.json")
+    suspend fun getConstructorStandings(): ConstructorStandingsWrapper
 }
---
+++ core/network/src/main/java/com/toquete/boxbox/network/model/ConstructorStandingsResponse.kt
@@ -0,0 +1,12 @@
+package com.toquete.boxbox.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+typealias ConstructorStandingsWrapper = DataResponse<StandingsTableResponse<StandingsLists<ConstructorStandingsResponse>>>
+
+@Serializable
+data class ConstructorStandingsResponse(
+    @SerialName("ConstructorStandings")
+    val constructorStandings: List<ConstructorResponse>
+)
