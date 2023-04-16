b060cfb66184a02b1a7a9aca3859441cfffc8a03
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-04-16 18:25:45 -0300

    Exporting database

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -6,8 +6,29 @@ plugins {
 
 android {
     namespace = "com.toquete.boxbox.database"
+
+    defaultConfig {
+        javaCompileOptions {
+            annotationProcessorOptions {
+                compilerArgumentProviders(
+                    RoomSchemaArgProvider(File(projectDir, "schemas"))
+                )
+            }
+        }
+    }
 }
 
 dependencies {
     implementation(project(":core:model"))
+}
+
+class RoomSchemaArgProvider(
+    @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    val schemaDir: File
+) : CommandLineArgumentProvider {
+
+    override fun asArguments(): Iterable<String> {
+        return listOf("-Aroom.schemaLocation=${schemaDir.path}")
+    }
 }
