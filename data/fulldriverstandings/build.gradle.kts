9e6badfbf0979b7e01e07b47df690357dc189763
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-04-21 09:29:50 -0300

    Creating full driver standings data module

---
+++ data/fulldriverstandings/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("boxbox.android.library")
+    id("boxbox.android.hilt")
+}
+
+android {
+    namespace = "com.toquete.boxbox.data.fulldriverstandings"
+}
+
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:database"))
+    implementation(project(":core:preferences"))
+    implementation(project(":core:network"))
+}
