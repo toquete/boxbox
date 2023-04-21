90dab057119f985bcbd60c0e17fe5d21774a88cf
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-04-21 10:19:15 -0300

    Creating drivers data module

---
+++ data/drivers/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("boxbox.android.library")
+    id("boxbox.android.hilt")
+}
+
+android {
+    namespace = "com.toquete.boxbox.data.drivers"
+}
+
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:database"))
+}
