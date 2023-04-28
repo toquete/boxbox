ee48969c765fdddf470985b456c0e17d13cb6686
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-04-28 18:53:42 -0300

    Adding syncing indicator

--- app/src/main/java/com/toquete/boxbox/MainActivity.kt
+++ app/src/main/java/com/toquete/boxbox/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
@@ -32,6 +36,7 @@ import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -117,6 +122,13 @@ private fun MainScreenContent(isOnline: Boolean, isSyncing: Boolean) {
         },
         content = { paddingValues ->
             Column(modifier = Modifier.padding(paddingValues)) {
+                AnimatedVisibility(visible = isSyncing) {
+                    LinearProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(4.dp)
+                    )
+                }
                 TabRow(selectedTabIndex = selectedTabIndex) {
                     tabTitles.forEachIndexed { index, title ->
                         Tab(
