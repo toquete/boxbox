1fad252f344594e5facd0264e8346909412976df
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-04-29 13:32:59 -0300

    Removing unused class

--- app/src/main/java/com/toquete/boxbox/StandingsTab.kt
+++
@@ -1,8 +0,0 @@
-package com.toquete.boxbox
-
-import androidx.annotation.StringRes
-
-enum class StandingsTab(@StringRes val titleId: Int) {
-    DRIVERS(R.string.drivers),
-    CONSTRUCTORS(R.string.constructors)
-}
