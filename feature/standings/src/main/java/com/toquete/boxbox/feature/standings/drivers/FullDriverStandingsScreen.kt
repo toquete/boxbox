4ef0dd5e8f8643df3986fc8fcb52f79d7f9d5af4
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-04-29 14:03:19 -0300

    Implementing fab on drivers screen

--- feature/standings/src/main/java/com/toquete/boxbox/feature/standings/drivers/FullDriverStandingsScreen.kt
+++ feature/standings/src/main/java/com/toquete/boxbox/feature/standings/drivers/FullDriverStandingsScreen.kt
@@ -1,14 +1,30 @@
 package com.toquete.boxbox.feature.standings.drivers
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowUpward
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,16 +32,44 @@ import com.toquete.boxbox.core.ui.theme.BoxBoxTheme
 import com.toquete.boxbox.model.Constructor
 import com.toquete.boxbox.model.Driver
 import com.toquete.boxbox.model.FullDriverStanding
+import kotlinx.coroutines.launch
 
 @Composable
 fun FullDriverStandingsScreen(list: List<FullDriverStanding>) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        items(list) { standing ->
-            FullDriverStandingItem(standing)
+    val lazyListState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val showButton by remember {
+        derivedStateOf { lazyListState.firstVisibleItemIndex > 0 }
+    }
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            state = lazyListState
+        ) {
+            items(list) { standing ->
+                FullDriverStandingItem(standing)
+            }
+        }
+        AnimatedVisibility(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
+            visible = showButton,
+            enter = scaleIn(),
+            exit = scaleOut()
+        ) {
+            FloatingActionButton(
+                onClick = {
+                    coroutineScope.launch {
+                        lazyListState.animateScrollToItem(index = 0)
+                    }
+                },
+                shape = FloatingActionButtonDefaults.largeShape
+            ) {
+                Icon(imageVector = Icons.Default.ArrowUpward, contentDescription = null)
+            }
         }
     }
 }
