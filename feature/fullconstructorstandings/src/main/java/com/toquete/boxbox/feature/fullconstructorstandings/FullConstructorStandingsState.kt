1a676f6d7af62ad9cd2942dbc2bff85d588565a6
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-04-29 13:28:04 -0300

    Removing old modules

--- feature/fullconstructorstandings/src/main/java/com/toquete/boxbox/feature/fullconstructorstandings/FullConstructorStandingsState.kt
+++
@@ -1,7 +0,0 @@
-package com.toquete.boxbox.feature.fullconstructorstandings
-
-import com.toquete.boxbox.model.FullConstructorStanding
-
-internal sealed interface FullConstructorStandingsState {
-    data class Success(val data: List<FullConstructorStanding>) : FullConstructorStandingsState
-}
