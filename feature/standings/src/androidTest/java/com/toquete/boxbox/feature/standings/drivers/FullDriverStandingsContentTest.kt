a7c41bd0c54465eb9e9644b3710e3799fff146ad
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-05-04 17:02:09 -0300

    Implementing FullDriverStandingContent test

---
+++ feature/standings/src/androidTest/java/com/toquete/boxbox/feature/standings/drivers/FullDriverStandingsContentTest.kt
@@ -0,0 +1,70 @@
+package com.toquete.boxbox.feature.standings.drivers
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.filterToOne
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollToIndex
+import com.toquete.boxbox.core.testing.data.fullDriverStandings
+import org.junit.Rule
+import org.junit.Test
+
+class FullDriverStandingsContentTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun validateScrollButtonIsNotVisibleWhenScreenStarts() {
+        val list = fullDriverStandings.toMutableList()
+        (1..10).forEach { _ -> list.add(list.first().copy()) }
+
+        with(composeTestRule) {
+            setContent {
+                FullDriverStandingsContent(FullDriversStandingsState.Success(list))
+            }
+
+            onNodeWithTag("Scroll Button").assertDoesNotExist()
+        }
+    }
+
+    @Test
+    fun validateScrollButtonIsVisibleWhenScreenIsScrolled() {
+        val list = fullDriverStandings.toMutableList()
+        (1..10).forEach { _ -> list.add(list.first().copy()) }
+
+        with(composeTestRule) {
+            setContent {
+                FullDriverStandingsContent(FullDriversStandingsState.Success(list))
+            }
+
+            onNodeWithTag("Driver Standings List").performScrollToIndex(1)
+            onNodeWithTag("Scroll Button").assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun validateScrollButtonClick() {
+        val list = fullDriverStandings.toMutableList()
+        (2..10).forEach { list.add(list.first().copy(position = it)) }
+
+        with(composeTestRule) {
+            setContent {
+                FullDriverStandingsContent(FullDriversStandingsState.Success(list))
+            }
+
+            onNodeWithTag("Driver Standings List").performScrollToIndex(9)
+            onNodeWithTag("Scroll Button").performClick()
+            onNodeWithTag("Driver Standings List")
+                .onChildren()
+                .onFirst()
+                .onChildren()
+                .filterToOne(hasText("1"))
+                .assertIsDisplayed()
+        }
+    }
+}
