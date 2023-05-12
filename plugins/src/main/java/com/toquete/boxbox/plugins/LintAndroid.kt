edaffd6a9465955f9f50df328d00797784955ff6
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-05-12 08:08:41 -0300

    Configuring lint

--- plugins/src/main/java/com/toquete/boxbox/plugins/LintAndroid.kt
+++ plugins/src/main/java/com/toquete/boxbox/plugins/LintAndroid.kt
@@ -3,6 +3,11 @@ package com.toquete.boxbox.plugins
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 
+private fun String.toProjectName(): String {
+    return replaceFirst(":", "")
+        .replace(":", "-")
+}
+
 internal fun Project.configureLintAndroid(
     commonExtension: CommonExtension<*, *, *, *>,
 ) {
@@ -12,7 +17,7 @@ internal fun Project.configureLintAndroid(
             xmlReport = false
             sarifReport = false
             htmlReport = true
-            htmlOutput = file("$buildDir/reports/${project.path}-lint-report.html")
+            htmlOutput = file("$buildDir/reports/${project.path.toProjectName()}-lint-report.html")
         }
     }
 }
