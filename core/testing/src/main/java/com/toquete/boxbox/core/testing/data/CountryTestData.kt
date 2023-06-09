64c403dbeecad3cec7f97fc48daa9f04ef6f0e7f
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-06-09 14:40:27 -0300

    Creating remote country data source tests

--- core/testing/src/main/java/com/toquete/boxbox/core/testing/data/CountryTestData.kt
+++ core/testing/src/main/java/com/toquete/boxbox/core/testing/data/CountryTestData.kt
@@ -1,6 +1,7 @@
 package com.toquete.boxbox.core.testing.data
 
 import com.toquete.boxbox.core.database.model.CountryEntity
+import com.toquete.boxbox.core.network.model.CountryResponse
 
 val countryEntities = listOf(
     CountryEntity(
@@ -10,6 +11,21 @@ val countryEntities = listOf(
         flagUrl = "http://flag.com"
     ),
     CountryEntity(
+        id = "AUT",
+        name = "Austria",
+        nationality = "Austrian",
+        flagUrl = "http://flag.com"
+    )
+)
+
+val countryResponses = listOf(
+    CountryResponse(
+        id = "NED",
+        name = "Netherlands",
+        nationality = "Dutch",
+        flagUrl = "http://flag.com"
+    ),
+    CountryResponse(
         id = "AUT",
         name = "Austria",
         nationality = "Austrian",
