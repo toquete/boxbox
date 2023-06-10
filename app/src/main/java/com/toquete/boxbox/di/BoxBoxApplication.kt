4eb4fe631a4057a4f43566a977a28df9abb5acc0
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-06-10 11:56:37 -0300

    Adding Timber to project

--- app/src/main/java/com/toquete/boxbox/di/BoxBoxApplication.kt
+++ app/src/main/java/com/toquete/boxbox/di/BoxBoxApplication.kt
@@ -5,8 +5,10 @@ import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import androidx.work.ExistingWorkPolicy
 import androidx.work.WorkManager
+import com.toquete.boxbox.BuildConfig
 import com.toquete.boxbox.worker.SyncWorker
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 import javax.inject.Inject
 
 const val SYNC_WORK_NAME = "SYNC_WORK_NAME"
@@ -17,8 +19,16 @@ class BoxBoxApplication : Application(), Configuration.Provider {
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
+    @Inject
+    lateinit var crashlyticsReportingTree: Timber.Tree
+
     override fun onCreate() {
         super.onCreate()
+        setupSyncWork()
+        setupTimber()
+    }
+
+    private fun setupSyncWork() {
         WorkManager.getInstance(this)
             .enqueueUniqueWork(
                 SYNC_WORK_NAME,
@@ -26,6 +36,15 @@ class BoxBoxApplication : Application(), Configuration.Provider {
                 SyncWorker.setupWork()
             )
     }
+
+    private fun setupTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(crashlyticsReportingTree)
+        }
+    }
+
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder()
             .setWorkerFactory(workerFactory)
