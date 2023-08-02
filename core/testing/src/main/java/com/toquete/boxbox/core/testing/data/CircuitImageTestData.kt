dd2329348aa03e54c521909f1e5d9a005e6e8cc3
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-08-02 13:16:04 -0300

    Creating circuit image remote data source

---
+++ core/testing/src/main/java/com/toquete/boxbox/core/testing/data/CircuitImageTestData.kt
@@ -0,0 +1,10 @@
+package com.toquete.boxbox.core.testing.data
+
+import com.toquete.boxbox.core.network.model.CircuitImageResponse
+
+val circuitImageResponses = listOf(
+    CircuitImageResponse(
+        id = "bahrain",
+        imageUrl = "http://image.com"
+    )
+)
