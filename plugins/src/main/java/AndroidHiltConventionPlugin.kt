250aa587d8c32015e7792b6b6f3fd269aa0bfeb1
Guilherme Toquete <guilherme.toquete@gmail.com>
2024-01-24 18:13:52 -0300

    Updating hilt-work to 1.1.0

--- plugins/src/main/java/AndroidHiltConventionPlugin.kt
+++ plugins/src/main/java/AndroidHiltConventionPlugin.kt
@@ -1,10 +1,8 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 class AndroidHiltConventionPlugin : Plugin<Project> {
 
@@ -12,17 +10,13 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("dagger.hilt.android.plugin")
-                apply("org.jetbrains.kotlin.kapt")
-            }
-
-            extensions.configure<KaptExtension> {
-                correctErrorTypes = true
+                apply("com.google.devtools.ksp")
             }
 
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
                 add("implementation", libs.findLibrary("hilt").get())
-                add("kapt", libs.findLibrary("hilt.compiler").get())
+                add("ksp", libs.findLibrary("hilt.compiler").get())
             }
         }
     }
