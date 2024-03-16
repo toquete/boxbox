41a6822908e343c8c97ae084103c56abf84c4bf3
Guilherme Toquete <guilherme.toquete@gmail.com>
2024-03-15 23:52:43 -0300

    Creating podiums query

--- core/database/src/main/java/com/toquete/boxbox/core/database/dao/RaceResultDao.kt
+++ core/database/src/main/java/com/toquete/boxbox/core/database/dao/RaceResultDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Query
 import androidx.room.Transaction
 import androidx.room.Upsert
 import com.toquete.boxbox.core.database.model.RaceResultEntity
+import com.toquete.boxbox.core.database.model.RaceResultWithCircuitAndDriverEntity
 import com.toquete.boxbox.core.database.model.RaceResultWithDriverAndConstructorEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -19,6 +20,23 @@ interface RaceResultDao {
             "ORDER BY position ASC"
     )
     fun getRaceResultsBySeasonAndRound(season: String, round: Int): Flow<List<RaceResultWithDriverAndConstructorEntity>>
+
+    @Query(
+        "SELECT results.*, circuits.country, circuits.circuitName, drivers.code, images.image_url " +
+                "FROM race_results AS results " +
+                "INNER JOIN races " +
+                "ON races.season = results.season " +
+                "AND races.round = results.round " +
+                "INNER JOIN drivers " +
+                "ON drivers.id = results.driver_id " +
+                "INNER JOIN circuits " +
+                "ON circuits.id = races.circuit_id " +
+                "LEFT JOIN drivers_images AS images " +
+                "ON images.id = results.driver_id " +
+                "WHERE results.season = :season " +
+                "AND results.position BETWEEN 1 AND 3"
+    )
+    fun getRaceResultsPodiumsBySeason(season: String): Flow<List<RaceResultWithCircuitAndDriverEntity>>
 
     @Upsert
     suspend fun upsertAll(raceResults: List<RaceResultEntity>)
