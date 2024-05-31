b54cf3db2b7bdb875856a7115341efca968b5083
Guilherme Toquete <guilherme.toquete@gmail.com>
2024-05-31 14:50:35 -0300

    Replacing TabRow with SegmentedButton

--- feature/raceresults/src/main/java/com/toquete/boxbox/feature/raceresults/ui/RaceAndSprintResult.kt
+++ feature/raceresults/src/main/java/com/toquete/boxbox/feature/raceresults/ui/RaceAndSprintResult.kt
@@ -2,12 +2,16 @@ package com.toquete.boxbox.feature.raceresults.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,7 +21,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.toquete.boxbox.core.model.Constructor
 import com.toquete.boxbox.core.model.Driver
 import com.toquete.boxbox.core.model.RaceResult
@@ -26,7 +32,7 @@ import com.toquete.boxbox.core.ui.theme.BoxBoxTheme
 import com.toquete.boxbox.feature.raceresults.model.RaceResultsTab
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun RaceAndSprintResult(
     raceResults: List<RaceResult>,
@@ -46,20 +52,29 @@ fun RaceAndSprintResult(
     }
 
     Column {
-        TabRow(selectedTabIndex = pages.indexOf(selectedTab)) {
+        SingleChoiceSegmentedButtonRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        ) {
             pages.forEachIndexed { index, raceResultsTab ->
-                Tab(
-                    selected = selectedTab == raceResultsTab,
+                SegmentedButton(
+                    selected = index == pages.indexOf(selectedTab),
                     onClick = {
                         coroutineScope.launch {
                             pagerState.animateScrollToPage(index)
                         }
                     },
-                    text = { Text(stringResource(raceResultsTab.titleId)) }
-                )
+                    shape = SegmentedButtonDefaults.itemShape(index = index, count = pages.size)
+                ) {
+                    Text(text = stringResource(raceResultsTab.titleId))
+                }
             }
         }
-        HorizontalPager(state = pagerState) { page ->
+        HorizontalPager(
+            state = pagerState,
+            userScrollEnabled = false
+        ) { page ->
             when (pages[page]) {
                 RaceResultsTab.RACE -> RaceResultTable(raceResults)
                 RaceResultsTab.SPRINT -> RaceResultTable(sprintResults)
