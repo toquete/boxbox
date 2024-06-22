b10521c0e383350d73e89c8809f8919d00fb8e7e
Guilherme Toquete <guilherme.toquete@gmail.com>
2024-06-22 08:42:32 -0300

    Removing Instant deprecated code

--- feature/races/src/main/java/com/toquete/boxbox/feature/races/ui/RaceItem.kt
+++ feature/races/src/main/java/com/toquete/boxbox/feature/races/ui/RaceItem.kt
@@ -36,7 +36,7 @@ import com.toquete.boxbox.core.ui.annotation.UiModePreviews
 import com.toquete.boxbox.core.ui.custom.BoxBoxAsyncImage
 import com.toquete.boxbox.core.ui.theme.BoxBoxTheme
 import com.toquete.boxbox.core.ui.theme.FormulaOne
-import kotlinx.datetime.toInstant
+import kotlinx.datetime.Instant
 import com.toquete.boxbox.core.ui.R as uiR
 
 @Composable
@@ -172,11 +172,11 @@ internal fun RaceItemPreview() {
                     flagUrl = null,
                     imageUrl = null
                 ),
-                dateTime = "2023-03-05T15:00:00Z".toInstant(),
-                firstPracticeDateTime = "2023-03-03T11:30:00Z".toInstant(),
-                secondPracticeDateTime = "2023-03-03T15:00:00Z".toInstant(),
-                thirdPracticeDateTime = "2023-03-04T11:30:00Z".toInstant(),
-                qualifyingDateTime = "2023-03-04T15:00:00Z".toInstant(),
+                dateTime = Instant.parse("2023-03-05T15:00:00Z"),
+                firstPracticeDateTime = Instant.parse("2023-03-03T11:30:00Z"),
+                secondPracticeDateTime = Instant.parse("2023-03-03T15:00:00Z"),
+                thirdPracticeDateTime = Instant.parse("2023-03-04T11:30:00Z"),
+                qualifyingDateTime = Instant.parse("2023-03-04T15:00:00Z"),
                 sprintDateTime = null
             )
         )
