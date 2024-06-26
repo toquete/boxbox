6d0dabf916e21e0ead304ec57bda0615b7bdd85b
Guilherme Toquete <guilherme.toquete@gmail.com>
2024-06-26 19:14:46 -0300

    Removing notification builder

--- core/notification/src/main/java/com/toquete/boxbox/core/notification/DefaultNotificationHelper.kt
+++ core/notification/src/main/java/com/toquete/boxbox/core/notification/DefaultNotificationHelper.kt
@@ -1,17 +1,12 @@
 package com.toquete.boxbox.core.notification
 
 import android.app.NotificationChannel
-import android.content.Context
 import android.os.Build
-import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.toquete.boxbox.core.notification.model.Notification
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
-import android.app.Notification as AndroidNotification
 
 internal class DefaultNotificationHelper @Inject constructor(
-    @ApplicationContext private val context: Context,
     private val notificationManager: NotificationManagerCompat
 ) : NotificationHelper {
 
@@ -19,19 +14,8 @@ internal class DefaultNotificationHelper @Inject constructor(
         createNotificationChannel(notification)
 
         if (notificationManager.areNotificationsEnabled()) {
-            notificationManager.notify(notification.id, buildNotification(notification))
+            notificationManager.notify(notification.id, notification.notification)
         }
-    }
-
-    private fun buildNotification(notification: Notification): AndroidNotification {
-        return NotificationCompat.Builder(context, notification.channel.id)
-            .setSmallIcon(notification.smallIcon)
-            .setContentTitle(notification.title)
-            .setContentText(notification.text)
-            .setPriority(notification.priority)
-            .setCategory(notification.category)
-            .setStyle(notification.style)
-            .build()
     }
 
     private fun createNotificationChannel(notification: Notification) {
