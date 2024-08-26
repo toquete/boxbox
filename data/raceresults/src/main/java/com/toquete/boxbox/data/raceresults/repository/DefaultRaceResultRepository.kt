a1583045f965f55b13b9337c1f8d84c81197ad32
Guilherme Toquete <guilherme.toquete@gmail.com>
2024-08-26 12:49:01 -0300

    Adding withContext to DefaultRaceResultRepository

--- data/raceresults/src/main/java/com/toquete/boxbox/data/raceresults/repository/DefaultRaceResultRepository.kt
+++ data/raceresults/src/main/java/com/toquete/boxbox/data/raceresults/repository/DefaultRaceResultRepository.kt
@@ -1,5 +1,6 @@
 package com.toquete.boxbox.data.raceresults.repository
 
+import com.toquete.boxbox.core.common.annotation.IoDispatcher
 import com.toquete.boxbox.core.database.model.RaceResultWithDriverAndConstructorEntity
 import com.toquete.boxbox.core.model.RaceResult
 import com.toquete.boxbox.data.raceresults.model.toDomain
@@ -7,23 +8,28 @@ import com.toquete.boxbox.data.raceresults.model.toEntity
 import com.toquete.boxbox.data.raceresults.source.local.RaceResultLocalDataSource
 import com.toquete.boxbox.data.raceresults.source.remote.RaceResultRemoteDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 internal class DefaultRaceResultRepository @Inject constructor(
     private val remoteDataSource: RaceResultRemoteDataSource,
-    private val localDataSource: RaceResultLocalDataSource
+    private val localDataSource: RaceResultLocalDataSource,
+    @IoDispatcher private val dispatcher: CoroutineContext
 ) : RaceResultRepository {
 
     override fun getRaceResultsBySeasonAndRound(season: String, round: Int): Flow<List<RaceResult>> {
         return localDataSource.getRaceResultsBySeasonAndRound(season, round)
             .map { it.map(RaceResultWithDriverAndConstructorEntity::toDomain) }
+            .flowOn(dispatcher)
     }
 
     override suspend fun sync() {
-        remoteDataSource.getRaceResults()
-            .also { list ->
-                localDataSource.insertAll(list.toEntity())
-            }
+        withContext(dispatcher) {
+            val list = remoteDataSource.getRaceResults()
+            localDataSource.insertAll(list.toEntity())
+        }
     }
 }
