e9a18535165adcd692f4762a69be7cd8ea7e080c
Guilherme Toquete <guilherme.toquete@gmail.com>
2024-11-19 15:04:04 -0300

    Removing CircuitImageLocalDataSource

--- data/circuitimages/src/main/java/com/toquete/boxbox/data/circuitimages/source/local/CircuitImageLocalDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.toquete.boxbox.data.circuitimages.source.local
-
-import com.toquete.boxbox.core.database.model.CircuitImageEntity
-
-internal interface CircuitImageLocalDataSource {
-
-    suspend fun insertAll(circuitImages: List<CircuitImageEntity>)
-}
