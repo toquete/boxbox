d1387b7253673fbd7ef7d8a18f45ac9160ccaa5b
Guilherme Toquete <guilherme.toquete@gmail.com>
2024-11-19 22:20:19 -0300

    Inverting dependency raceresults

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -11,7 +11,6 @@ dependencies {
     api(project(":core:model"))
     api(project(":core:common"))
     implementation(project(":data:races"))
-    implementation(project(":data:raceresults"))
     implementation(project(":data:sprintresults"))
 
     testImplementation(project(":core:testing"))
