73af895b948fa99d0f5f2ab71408468b22ae99aa
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-05-08 17:18:40 -0300

    Refactor use case classes to remove Hilt dependencies

--- domain/src/main/java/com/toquete/boxbox/domain/usecase/GetTodayLocalDateUseCase.kt
+++ domain/src/main/java/com/toquete/boxbox/domain/usecase/GetTodayLocalDateUseCase.kt
@@ -3,9 +3,8 @@ package com.toquete.boxbox.domain.usecase
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.todayIn
-import javax.inject.Inject
 
-class GetTodayLocalDateUseCase @Inject constructor(private val clock: Clock) {
+class GetTodayLocalDateUseCase(private val clock: Clock) {
 
     operator fun invoke() = clock.todayIn(TimeZone.currentSystemDefault())
 }
