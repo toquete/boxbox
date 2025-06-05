c424674ffe99a9c5650d9e0b6cc42c91f6ffd19c
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-06-05 10:25:49 -0300

    Convert model module to Kotlin Multiplatform and update build configuration

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -1,11 +1,29 @@
+import com.android.build.api.dsl.androidLibrary
+
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
 }
 
 kotlin {
-    jvmToolchain(17)
-}
+    androidLibrary {
+        namespace = "com.toquete.boxbox.core.model"
+        compileSdk = 35
+        minSdk = 24
+    }
 
-dependencies {
-    implementation(libs.kotlinx.datetime)
+    listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach {
+        it.binaries.framework {
+            baseName = "modelKit"
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlin.stdlib)
+                implementation(libs.kotlinx.datetime)
+            }
+        }
+    }
 }
