83e7125b875fc3dc6eb7fa183cf894eb7929a933
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-06-05 10:35:01 -0300

    Refactor build configuration for Kotlin Multiplatform by removing Android library setup and adding JVM target

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -1,17 +1,9 @@
-import com.android.build.api.dsl.androidLibrary
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.kotlin.multiplatform.library)
 }
 
 kotlin {
-    androidLibrary {
-        namespace = "com.toquete.boxbox.core.model"
-        compileSdk = 35
-        minSdk = 24
-    }
-
+    jvm()
     listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach {
         it.binaries.framework {
             baseName = "modelKit"
