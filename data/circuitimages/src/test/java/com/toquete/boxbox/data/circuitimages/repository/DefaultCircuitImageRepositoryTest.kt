b841d64e3bcb24c29c60e071766f5899d01fa6dd
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-09-05 14:35:56 -0300

    Add circuit images retrieval and testing; implement getCircuitImages method and corresponding tests

--- data/circuitimages/src/test/java/com/toquete/boxbox/data/circuitimages/repository/DefaultCircuitImageRepositoryTest.kt
+++ data/circuitimages/src/test/java/com/toquete/boxbox/data/circuitimages/repository/DefaultCircuitImageRepositoryTest.kt
@@ -2,40 +2,32 @@ package com.toquete.boxbox.data.circuitimages.repository
 
 import com.toquete.boxbox.core.database.dao.CircuitImageDao
 import com.toquete.boxbox.core.testing.data.circuitImageEntities
-import com.toquete.boxbox.core.testing.data.circuitImageResponses
+import com.toquete.boxbox.data.circuitimages.fake.FakeCircuitImageDao
+import com.toquete.boxbox.data.circuitimages.fake.FakeCircuitImageRemoteDataSource
 import com.toquete.boxbox.data.circuitimages.source.remote.CircuitImageRemoteDataSource
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.mockk
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
-import java.io.IOException
+import kotlin.test.assertContentEquals
 
 class DefaultCircuitImageRepositoryTest {
 
-    private val remoteDataSource: CircuitImageRemoteDataSource = mockk(relaxed = true)
-    private val circuitImageDao: CircuitImageDao = mockk(relaxed = true)
-    private val repository = DefaultCircuitImageRepository(remoteDataSource, circuitImageDao)
+    private lateinit var remoteDataSource: CircuitImageRemoteDataSource
+    private lateinit var circuitImageDao: CircuitImageDao
+    private lateinit var repository: DefaultCircuitImageRepository
+
+    @Before
+    fun setUp() {
+        remoteDataSource = FakeCircuitImageRemoteDataSource()
+        circuitImageDao = FakeCircuitImageDao()
+        repository = DefaultCircuitImageRepository(remoteDataSource, circuitImageDao)
+    }
 
     @Test
     fun `sync should insert data in database when remote data is gotten successfully`() = runTest {
-        coEvery { remoteDataSource.getCircuitImages() } returns circuitImageResponses
-        coEvery { circuitImageDao.upsertAll(any()) } returns Unit
-
         repository.sync()
 
-        coVerify {
-            remoteDataSource.getCircuitImages()
-            circuitImageDao.upsertAll(circuitImageEntities)
-        }
-    }
-
-    @Test(expected = IOException::class)
-    fun `sync should not call local data source when remote data returns error`() = runTest {
-        coEvery { remoteDataSource.getCircuitImages() } throws IOException()
-
-        repository.sync()
-
-        coVerify(exactly = 0) { circuitImageDao.upsertAll(any()) }
+        assertContentEquals(circuitImageEntities, circuitImageDao.getCircuitImages().first())
     }
 }
