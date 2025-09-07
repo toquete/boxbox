0f2881b240be8e239d8d5555040916af63a37d7a
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-09-07 14:58:19 -0300

    Add fake database implementation for constructor images; refactor tests to use it

--- data/constructorimages/src/test/java/com/toquete/boxbox/data/constructorimages/source/remote/DefaultConstructorImageRemoteDataSourceTest.kt
+++ data/constructorimages/src/test/java/com/toquete/boxbox/data/constructorimages/source/remote/DefaultConstructorImageRemoteDataSourceTest.kt
@@ -2,21 +2,25 @@ package com.toquete.boxbox.data.constructorimages.source.remote
 
 import com.toquete.boxbox.core.network.BoxBoxRemoteDatabase
 import com.toquete.boxbox.core.testing.data.constructorImageResponses
-import io.mockk.coEvery
-import io.mockk.mockk
+import com.toquete.boxbox.data.constructorimages.fake.FakeBoxBoxRemoteDatabase
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertContentEquals
 
 class DefaultConstructorImageRemoteDataSourceTest {
 
-    private val remoteDatabase: BoxBoxRemoteDatabase = mockk(relaxed = true)
-    private val dataSource = DefaultConstructorImageRemoteDataSource(remoteDatabase)
+    private lateinit var remoteDatabase: BoxBoxRemoteDatabase
+    private lateinit var dataSource: DefaultConstructorImageRemoteDataSource
+
+    @Before
+    fun setup() {
+        remoteDatabase = FakeBoxBoxRemoteDatabase()
+        dataSource = DefaultConstructorImageRemoteDataSource(remoteDatabase)
+    }
 
     @Test
     fun `getConstructorsImages should return all constructors images from remote when called`() = runTest {
-        coEvery { remoteDatabase.getConstructorImages() } returns constructorImageResponses
-
         val result = dataSource.getConstructorsImages()
 
         assertContentEquals(constructorImageResponses, result)
