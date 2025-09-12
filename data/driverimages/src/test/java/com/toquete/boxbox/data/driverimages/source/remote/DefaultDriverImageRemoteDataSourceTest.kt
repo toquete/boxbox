5caf5b61505c4c13b4bb9d37e57cca585a0bd803
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-09-12 10:40:33 -0300

    Add FakeBoxBoxRemoteDatabase for testing; refactor DefaultDriverImageRemoteDataSourceTest to use fake implementation

--- data/driverimages/src/test/java/com/toquete/boxbox/data/driverimages/source/remote/DefaultDriverImageRemoteDataSourceTest.kt
+++ data/driverimages/src/test/java/com/toquete/boxbox/data/driverimages/source/remote/DefaultDriverImageRemoteDataSourceTest.kt
@@ -1,22 +1,26 @@
 package com.toquete.boxbox.data.driverimages.source.remote
 
 import com.toquete.boxbox.core.network.BoxBoxRemoteDatabase
+import com.toquete.boxbox.data.driverimages.fake.FakeBoxBoxRemoteDatabase
 import com.toquete.boxbox.data.driverimages.mock.driverImageResponses
-import io.mockk.coEvery
-import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertContentEquals
 
 class DefaultDriverImageRemoteDataSourceTest {
 
-    private val remoteDatabase: BoxBoxRemoteDatabase = mockk(relaxed = true)
-    private val dataSource = DefaultDriverImageRemoteDataSource(remoteDatabase)
+    private lateinit var remoteDatabase: BoxBoxRemoteDatabase
+    private lateinit var dataSource: DefaultDriverImageRemoteDataSource
+
+    @Before
+    fun setUp() {
+        remoteDatabase = FakeBoxBoxRemoteDatabase()
+        dataSource = DefaultDriverImageRemoteDataSource(remoteDatabase)
+    }
 
     @Test
     fun `getDriversImages should return all drivers images from remote when called`() = runTest {
-        coEvery { remoteDatabase.getDriversImages() } returns driverImageResponses
-
         val result = dataSource.getDriversImages()
 
         assertContentEquals(driverImageResponses, result)
