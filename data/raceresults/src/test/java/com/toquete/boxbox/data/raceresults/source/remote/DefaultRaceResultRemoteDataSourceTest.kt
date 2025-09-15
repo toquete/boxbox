71563d5cf74c44028116f3bd85e1e3aca9430e3e
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-09-15 14:47:06 -0300

    Introduce FakeBoxBoxService for race results testing and refactor tests

--- data/raceresults/src/test/java/com/toquete/boxbox/data/raceresults/source/remote/DefaultRaceResultRemoteDataSourceTest.kt
+++ data/raceresults/src/test/java/com/toquete/boxbox/data/raceresults/source/remote/DefaultRaceResultRemoteDataSourceTest.kt
@@ -1,22 +1,27 @@
 package com.toquete.boxbox.data.raceresults.source.remote
 
 import com.toquete.boxbox.core.network.BoxBoxService
+import com.toquete.boxbox.data.raceresults.fake.FakeBoxBoxService
 import com.toquete.boxbox.data.raceresults.mock.raceResultWrapper
-import io.mockk.coEvery
-import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class DefaultRaceResultRemoteDataSourceTest {
 
-    private val service: BoxBoxService = mockk(relaxed = true)
-    private val dataSource = DefaultRaceResultRemoteDataSource(service)
+    private lateinit var service: BoxBoxService
+    private lateinit var dataSource: DefaultRaceResultRemoteDataSource
+
+    @Before
+    fun setUp() {
+        service = FakeBoxBoxService()
+        dataSource = DefaultRaceResultRemoteDataSource(service)
+    }
 
     @Test
     fun `getRaceResults should return race results`() = runTest {
         val offset = 0
-        coEvery { service.getRaceResults(offset) } returns raceResultWrapper
 
         val result = dataSource.getRaceResults(offset)
 
