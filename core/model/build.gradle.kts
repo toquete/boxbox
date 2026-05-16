480bb53f3ed038c23935a5888d5dc9c32eb079b2
Guilherme Toquete <guilherme.toquete@gmail.com>
2026-05-16 17:40:20 -0300

    Revert "feat(core:model): migrate to KMP (commonMain)" (#643)
    This reverts commit e1d5fcb5a1949854fb55c729d6be0a318262ced5.

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -1,7 +1,18 @@
 plugins {
-    id("boxbox.kotlin.multiplatform")
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
 }
 
-android {
-    namespace = "com.toquete.boxbox.core.model"
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+dependencies {
+    add("implementation", libs.findLibrary("kotlinx.datetime").get())
 }
